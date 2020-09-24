1bbe5eac4fab7e971840ed4c76f97de3e30717e5
duarte-bruno <bruno.duarte@podfy.com.br>
2020-09-23 21:36:36 -0300

    Project created.

---
+++ the-movie-db/App/AppDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.swift
+//  the-movie-db
+//
+//  Created by Bruno on 23/09/20.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "the_movie_db")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                print(error)
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                print(nserror)
+            }
+        }
+    }
+}
+
